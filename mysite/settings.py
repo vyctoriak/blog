22a00409bc7585972fe3afd6ba3830f010d79d8d
Vyctoria Karina <vyctoriak@gmail.com>
2023-02-02 14:27:58 +0100

    solve error to deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,5 +125,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static',
+STATIC_URL = '/static/',
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
