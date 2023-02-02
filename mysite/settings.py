5242caa00c5047ed34d6082eba0115fc043ed5a7
Vyctoria Karina <vyctoriak@gmail.com>
2023-02-02 14:35:12 +0100

    solve error to deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/',
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
